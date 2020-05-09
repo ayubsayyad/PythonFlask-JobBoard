6a7edff35db2896cf59b9740b0353c505b349bb3
root <root@DESKTOP-LFADNIL.localdomain>
2020-05-09 12:21:20 +0530

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,7 @@
 from flask import render_template, Flask
 
 app = Flask(__name__)
-
+@app.route('/')
+@app.route('/jobs')
 def jobs():
-    render_template('index.html')
+    return render_template('index.html')
